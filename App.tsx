d48649dd96732fef17772a41372290c60bf9cfa1
Cauê Almeida <nncl@live.com>
2023-03-24 23:20:04 -0300

    feat: Sets up Google Font Roboto

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator style={{ flex: 1 }} />}
     </ThemeProvider>
   );
 }
