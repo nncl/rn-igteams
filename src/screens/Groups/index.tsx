dd33bb798955090d0504339f435d154539648b05
Cauê Almeida <nncl@live.com>
2023-04-05 21:50:14 -0300

    feat(screens): Adds Highlight component into Groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,13 @@
 import { Container } from './styles';
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+
+      <Highlight title="Play" subtitle="Play with your groups" />
     </Container>
   );
 }
