716398d3508e89daf44802a9f83ec0341f47498d
Cauê Almeida <nncl@live.com>
2023-04-12 20:58:04 -0300

    feat(screens): Includes GroupCard within Groups' page

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,7 @@
 import { Container } from './styles';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
   return (
@@ -8,6 +9,8 @@ export function Groups() {
       <Header />
 
       <Highlight title="Play" subtitle="Play with your groups" />
+
+      <GroupCard title="Hello world" />
     </Container>
   );
 }
